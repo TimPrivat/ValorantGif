05f36d4af14e7857f6e5d6730d9bb79b9bfc30e7
BuildTools <unconfigured@null.spigotmc.org>
2022-12-07 21:10:26 +0100

    dada

---
+++ ValorantGifProject/src/main/java/ValorantGif/ValorantGifProject/ValorantGifProjectApplication.java
@@ -0,0 +1,13 @@
+package ValorantGif.ValorantGifProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantGifProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValorantGifProjectApplication.class, args);
+	}
+
+}
---
+++ ValorantGifProject/src/test/java/ValorantGif/ValorantGifProject/ValorantGifProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ValorantGif.ValorantGifProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorantGifProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
